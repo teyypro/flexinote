2047cdf242c9526ea82c4030ffb7093d63b9b853
worddo <worddolearning@gmail.com>
2025-07-09 11:12:58 +0700

    update

--- src/routes/ContentEditable.svelte
+++ src/routes/ContentEditable.svelte
@@ -1,56 +1,58 @@
 <script>
-    import { createEventDispatcher, onMount } from 'svelte';
-  
-    export let id;
-    export let content = '';
-  
-    const dispatch = createEventDispatcher();
-  
-    let element;
-  
-    function handleInput() {
-      content = element.innerHTML;
-      dispatch('contentChange', content);
-    }
-  
-    onMount(() => {
-      if (element) {
-        element.innerHTML = content;
-      }
-      return () => {
-        element = null;
-      };
-    });
-  </script>
-  
-  <div class="editor" spellcheck="false"
-    {id}
-    bind:this={element}
-    contenteditable="true"
-    on:input={handleInput}
-  />
-  
-  <style>
- .editor {
-    display: inline-block;
-    padding: .5rem;
-    min-width: 50%;
-    max-height: 80vh;
-    height: max-content;
-    width: max-content;
-    max-width: 96%;
-    background: #fff;
-    overflow: auto;
-    word-wrap: break-word;
-    border-left: 2px solid #43646d;
-    width: 96%;
-    transition: 0.1s ease;
+  import { createEventDispatcher, onMount } from 'svelte';
+
+  export let id;
+  export let content = '';
+
+  const dispatch = createEventDispatcher();
+
+  let element;
+
+  function handleInput() {
+    content = element.innerHTML;
+    dispatch('contentChange', content);
   }
 
-  .editor:hover, .editor:focus-visible, .editor:focus{
-    outline: 0;
-    border: 0;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    border-radius: 0.5rem;
-  }
-  </style>
+  onMount(() => {
+    if (element) {
+      element.innerHTML = content;
+    }
+    return () => {
+      element = null;
+    };
+  });
+</script>
+
+<div class="editor" spellcheck="false"
+  {id}
+  bind:this={element}
+  contenteditable="true"
+  on:input={handleInput}
+/>
+
+<style>
+.editor {
+  display: inline-block;
+  padding: .5rem;
+  min-width: 50%;
+  max-height: 80vh;
+  height: max-content;
+  width: max-content;
+  max-width: 96%;
+  background: #fff;
+  overflow: auto;
+  word-wrap: break-word;
+  border-left: 2px solid #43646d;
+  width: 96%;
+  transition: 0.1s ease;
+  cursor:text;
+  border-radius: 1vh;
+}
+
+.editor:hover, .editor:focus-visible, .editor:focus{
+  outline: 0;
+  border: 0;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 0.5rem;
+}
+</style>
